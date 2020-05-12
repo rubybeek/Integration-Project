bd53899756e8aa8e45cf72f0b61fb1371590bbe8
Willemijn-R <62099163+Willemijn-R@users.noreply.github.com>
2020-05-12 16:27:07 +0200

    plaatjes

--- Subspace identification 12-05/MAIN.m
+++ Subspace identification 12-05/MAIN.m
@@ -3,14 +3,35 @@ close all
 clearvars
 
 load('data.1.mat')
+datasmooth = smoothdata(data(:,1:2),'sgolay',60);
 
-yold = data(:,1:2);
+figure(1)
+subplot(2,1,1)
+plot(datasmooth(:,1))
+hold on
+plot(data(:,1))
+ylabel('Temperature (degC)')
+title('Smoothened Data Sensor 1')
+
+subplot(2,1,2)
+plot(datasmooth(:,2))
+hold on
+plot(data(:,2))
+title('Smoothened Data Sensor 2')
+ylabel('Temperature (degC)')
+xlabel('Time (sec)')
+
+dataset = iddata(datasmooth(:,1:2),data(:,3:4),1);
+dataset.InputName  = {'Input Sensor 1';'Input Sensor 2'};
+dataset.OutputName = {'Output Sensor 1';'Output Sensor 2'};
+
+%% subspace identification 
+yold = datasmooth(:,1:2);
 uold = data(:,3:4);
 N = length(yold);
 
 y = zeros(2*399,1);
 u = zeros(2*399,1);
-
 
 n = 2;
 s = n*4;
@@ -29,8 +50,24 @@ end
 
 [At, Bt, Ct, Dt, x0t, S, theta,Phi_N] = mysubid(y,u,s,n,yold);
 
+Ts = 1;
+sysd = ss(At,Bt,Ct,Dt,Ts);
+sysc = d2c(sysd);
 
+figure(2)
+compare(dataset,sysd);
+title('Subspace identification using own script')
 
+%% Subspace identification using ssest
+MIMO = ssest(dataset,2);
 
+figure(3)
+compare(dataset,MIMO);
+title('Subspace identification using ssest')
 
+%% Subspace identification using N4SID
+MIMO2 = n4sid(dataset,2);
 
+figure(4)
+compare(dataset,MIMO2);
+title('Subspace identification using N4SID')
--- Subspace identification 12-05/subidhelp2.m
+++ Subspace identification 12-05/subidhelp2.m
@@ -1,21 +1,21 @@
 function [Bt,Dt,x0t,theta,Phi_N]=subidhelp2(y,u,At,Ct,N,n,yold)
 
-L = 2;
-
 % Extract matrices Bt and Dt following section 9.2.4.3
 [ks,~] = size(Ct*At); 
 [~,Lu] = size(u);
 
+L = 2;
+Lu=2;
+
 Phi_N = zeros(N*ks,n + (Lu*n) + (Lu*L)); 
 
 for k = 1:N
-
+    
     clear Phi_1 Phi_2 Phi_3 
     
     Phi_2 = 0;
 
     Phi_1 = Ct*(At^k);
-
     
         for tau = 0:(k-1)
  
@@ -31,11 +31,10 @@ end
 
 Phi_N_T = Phi_N';
 
-theta = ((1/N)*Phi_N_T*Phi_N)\((1/N)*Phi_N_T.*yold(:,1));
+theta = ((1/N)*Phi_N_T*Phi_N)\((1/N)*Phi_N_T*y(:,1));
     
-x0t = theta(1:n,1);
-    
-Bt = reshape(theta((n+1):(n+(n*Lu))),n,Lu);
-    
+x0t = theta(1:n,1);   
+Bt = reshape(theta((n+1):(n+(n*Lu))),n,Lu);   
 Dt = reshape(theta(n+(n*Lu)+1:(n+(n*Lu)+ (Lu*L))),L,Lu);
+
 end
