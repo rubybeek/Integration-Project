082f59a4ff5bd623933bed0f7bedab43b2e31d1c
rubybeek <rubybeek@live.nl>
2020-05-29 11:37:57 +0200

    poleplacement

---
+++ Current Matlab/Control Design/poleplacement.m
@@ -0,0 +1,71 @@
+clearvars
+close all
+
+load('Linearization ss model discrete.mat');
+load('Linearization ss model continuous.mat');
+
+Ad = lin_discrete.A;
+Bd = lin_discrete.B;
+Cd = lin_discrete.C;
+Dd = lin_discrete.D;
+
+Ac = lin_statespace.A;
+Bc = lin_statespace.B;
+Cc = lin_statespace.C;
+Dc = lin_statespace.D;
+
+%% controllability test
+
+Co = ctrb(Ad,Bd);
+
+rankcheck = rank(Co);
+
+if rankcheck == min(size(Co))
+    display('System is controllable');
+else
+    display('System is NOT controllable');
+end
+
+
+%% Pole-placement
+
+% Check open loop poles
+
+P_OL = pole(lin_statespace);
+
+% Make closed loop
+figure(1)
+step(lin_statespace)
+
+% Solve for gain
+Kdc = dcgain(lin_statespace);
+Kr = inv(Kdc);
+
+
+%%
+
+figure(1)
+step(lin_discrete)
+
+figure(2)
+pzmap(lin_discrete)
+
+figure(3)
+pzmap(lin_statespace)
+
+K = place(Ac,Bc,[-5+3*j -5-3*j -10 -15]);
+
+closedloop_cont = feedback(G,K);
+
+figure(4)
+step(closedloop_cont)
+
+
+
+
+
+
+
+
+
+
